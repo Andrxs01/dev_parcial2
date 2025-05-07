fa2b2dc46b5a07e37794c065556314404a70ab02
Adrian <aabohorquez16@ucatolica.edu.co>
2025-05-06 21:28:35 -0500

    ÂFinalizacion

--- data/models.py
+++ data/models.py
@@ -3,6 +3,7 @@ from typing import Optional
 from enum import Enum
 from datetime import datetime
 
+
 class EstadoUsuario(str, Enum):
     activo = "Activo"
     inactivo = "Inactivo"
@@ -11,21 +12,23 @@ class EstadoUsuario(str, Enum):
 class Usuario(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
-    email: str
+    email: str = Field(unique=True, index=True)
     premium: bool = False
     estado: EstadoUsuario = Field(default=EstadoUsuario.activo)
 
 class EstadoTarea(str, Enum):
     pendiente = "pendiente"
-    en_ejecucion = "en ejecucion"
+    en_ejecucion = "en_ejecucion"
     realizada = "realizada"
     cancelada = "cancelada"
+
 
 class TareaBase(SQLModel):
     nombre: str
     descripcion: str
     estado: EstadoTarea = Field(default=EstadoTarea.pendiente)
-    usuario: int
+    usuario: int = Field(foreign_key="usuario.id", index=True)
+
 
 class Tarea(TareaBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -34,3 +37,7 @@ class Tarea(TareaBase, table=True):
 
 class TareaCreate(TareaBase):
     pass
+class TareaResponse(TareaBase):
+    id: int
+    fecha_creacion: datetime
+    fecha_modificacion: datetime
--- utils/db.py
+++ utils/db.py
@@ -2,24 +2,23 @@
 import os
 from dotenv import load_dotenv
 from sqlmodel import SQLModel, create_engine, Session
+from data.models import Usuario, Tarea
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///usuariosdb.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///tareadb.db")
 
 CLEVER_DB = None
 if all([os.getenv(f"CLEVER_{key}") for key in ["USER", "PASSWORD", "HOST", "PORT", "DATABASE"]]):
     CLEVER_DB = (
-        f"postgresql://{os.getenv('CLEVER_USER')}:"
-        f"{os.getenv('CLEVER_PASSWORD')}@"
-        f"{os.getenv('CLEVER_HOST')}:"
-        f"{os.getenv('CLEVER_PORT')}/"
+        f"postgresql://{os.getenv('CLEVER_USER')}:" 
+        f"{os.getenv('CLEVER_PASSWORD')}@" 
+        f"{os.getenv('CLEVER_HOST')}:" 
+        f"{os.getenv('CLEVER_PORT')}/" 
         f"{os.getenv('CLEVER_DATABASE')}"
     )
 
-
 FINAL_DB_URL = CLEVER_DB if CLEVER_DB else DATABASE_URL
-
 
 engine = create_engine(FINAL_DB_URL, echo=os.getenv("DEBUG", "False").lower() == "true")
 
